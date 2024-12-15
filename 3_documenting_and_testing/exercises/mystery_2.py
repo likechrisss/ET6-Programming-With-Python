cc77f9de75e455bf5e5afbf92701efb92bba260a
likechrisss <chrismylp1@gmail.com>
2024-12-14 20:14:06 -0500

    Completed exercice mystery_2 in 3_documenting_and_testing

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,5 +1,10 @@
-def mystery_2(a):
-    if len(a) == 0:
-        return None
+def mystery_2(a: list) -> int | None:
+    """Returns the length of a list or None if the list is empty.
 
-    return len(a)
+    Examples:
+        >>> mystery_2([])
+        None
+        >>> mystery_2([1, 2, 3])
+        3
+    """
+    return None if len(a) == 0 else len(a)
