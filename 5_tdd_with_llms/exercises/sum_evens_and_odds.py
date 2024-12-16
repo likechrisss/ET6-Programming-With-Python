4ddd5dd24b6dd3dc5643c2d12332fb591e4f1da6
likechrisss <chrismylp1@gmail.com>
2024-12-15 20:26:09 -0500

    Added the function to the exercise sum_evens_and_odds in 5_tdd_with_llms, test now passed, this is step 2

--- 5_tdd_with_llms/exercises/sum_evens_and_odds.py
+++ 5_tdd_with_llms/exercises/sum_evens_and_odds.py
@@ -1,6 +1,14 @@
-""" Sum Evens and Odds
+def sum_evens_and_odds(numbers):
+    """
+    Takes a list of numbers and returns a dictionary with 
+    the sums of even and odd numbers.
+    """
+    even_sum = 0
+    odd_sum = 0
+    for num in numbers:
+        if num % 2 == 0:
+            even_sum += num
+        else:
+            odd_sum += num
 
-Write a function that takes a list of numbers 
-and returns a dictionary with sums of the even and odd numbers in the list.
-
-"""
+    return {"even": even_sum, "odd": odd_sum}
