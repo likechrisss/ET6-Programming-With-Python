a4e85f48e17b6d7a8da2ffb694c28783d0c15a9a
Chrismy Augustin <chrismylp1@gmail.com>
2024-12-15 20:06:55 -0500

    Small spelling mistake

--- 5_tdd_with_llms/exercises/sort_numbers.py
+++ 5_tdd_with_llms/exercises/sort_numbers.py
@@ -1,7 +1,7 @@
 """ Sort Numbers
 
 Write a function that takes in a list of numbers
-It will return a new list with the same numbers from lowest to highets
+It will return a new list with the same numbers from lowest to highest
 -> this function does not have side effects
 
 """
